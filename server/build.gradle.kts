321f452cdf383a3937a66ebed537e53ed26a44ce
marcelo.filho <marceloasf1999@gmail.com>
2022-05-12 21:11:22 -0300

    Trocando JDK para 17

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "br.com"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
@@ -31,7 +31,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "17"
 	}
 }
 
