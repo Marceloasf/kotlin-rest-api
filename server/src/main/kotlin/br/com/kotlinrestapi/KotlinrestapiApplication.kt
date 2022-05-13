b7128266c9a86c26c4a1ac6b450c4c31f631be3a
marcelo.filho <marceloasf1999@gmail.com>
2022-05-12 21:02:54 -0300

    First commit after starting project

---
+++ server/src/main/kotlin/br/com/kotlinrestapi/KotlinrestapiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.kotlinrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinrestapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinrestapiApplication>(*args)
+}
---
+++ server/src/test/kotlin/br/com/kotlinrestapi/KotlinrestapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.kotlinrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinrestapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
